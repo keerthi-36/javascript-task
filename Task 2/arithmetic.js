809dd56a2395e95840f55d00ea7e1503d3e133c5
keerthi-36 <keerthikadurairaj36@gmail.com>
2026-05-18 19:17:45 +0530

    Task 2 completed

---
+++ Task 2/arithmetic.js
@@ -0,0 +1,168 @@
+// 1. Create a variable and store your favorite food.
+let favoriteFood = "Biryani";
+
+// 2. Create a variable and store your mobile number.
+let mobileNumber = "1234567890";
+
+// 3. Create a boolean variable with false value.
+let isRaining = false;
+
+// 4. Create one undefined variable and print typeof.
+  let data;
+    console.log(typeof data); // Output: undefined
+
+// 5. Create one array with 6 animal names.
+let animals = ["Dog", "Cat", "Elephant", "Lion", "Tiger", "Giraffe"];
+
+// 6. Print second animal from array.
+console.log(animals[1]);
+
+// 7. Print last animal from array using length.
+console.log(animals[animals.length-1]);
+
+// 8. Create object with bike details (name, model, color).
+let bike = {
+    name: "Yamaha",
+    model: "R15",
+    color: "Blue"
+};
+
+// 9. Print bike color from object.
+console.log(bike.color);
+
+
+// 10. Create two variables and subtract values.
+let a = 10;
+let b = 5;
+let sub = a - b;
+console.log(sub);
+
+// 11. Create two variables and divide values.
+let x = 20;
+let y = 4;
+let div = x / y;
+console.log(div);
+
+// 12. Find 15 % 2.
+let mod = 15 % 2;
+console.log(mod);
+
+// 13. Find 2 ** 5.
+let exp = 2 ** 5;
+console.log(exp);
+
+// 14. Create variable with 5 and use post increment.
+let num = 5;
+
+console.log(num++);
+
+// 15. Create variable with 10 and use pre increment.
+let num2 = 10;
+
+console.log(++num2);
+
+// 16. Create variable with 20 and use post decrement.
+let num3 = 20;
+
+console.log(num3--);
+
+// 17. Create variable with 50 and use pre decrement.
+let num4 = 50;
+
+console.log(--num4);
+
+// 18. Check 100 > 50.
+console.log(100 > 50); // Output: true
+
+// 19. Check 25 < 10.
+console.log(25 < 10); // Output: false
+
+// 20. Check 50 >= 50.
+console.log(50 >= 50); // Output: true
+
+// 21. Check 30 <= 25.
+console.log(30 <= 25); // Output: false
+
+// 22. Check 10 == "10".
+console.log(10 == "10"); // Output: true
+
+// 23. Check 10 === "10".
+console.log(10 === "10"); // Output: false
+
+// 24. Check 25 != "25".
+console.log(25 != "25"); // Output: false
+
+// 25. Check 25 !== "25".
+console.log(25 !== "25"); // Output: true
+
+// 26. Use AND operator:
+//     10 > 5 && 20 > 15
+console.log(10 > 5 && 20 > 15); // Output: true
+
+// 27. Use AND operator:
+//     5 > 10 && 20 > 15
+console.log(5 > 10 && 20 > 15); // Output: false
+
+// 28. Use OR operator:
+//     5 > 10 || 20 > 15
+console.log(5 > 10 || 20 > 15); // Output: true
+
+// 29. Use OR operator:
+//     2 > 5 || 1 > 10
+console.log(2 > 5 || 1 > 10); // Output: false
+
+// 30. Use NOT operator:
+//     !(10 > 5)
+console.log(!(10 > 5)); // Output: false
+
+// 31. Use NOT operator:
+//     !(5 > 10)
+console.log(!(5 > 10)); // Output: true
+
+// 32. Create ternary operator:
+//     if 50 > 25 print "True"
+//     else print "False"
+console.log(50>25?"true":"false");
+
+
+
+// 33. Create ternary operator:
+//     if 18 >= 21 print "Adult"
+//     else print "Minor"
+console.log(18 >= 21 ? "Adult" : "Minor");
+
+// 34. Create one array with 5 city names and print third city.
+let cities = ["New York", "London", "Paris", "Tokyo", "Sydney"];
+console.log(cities[2]);
+
+// 35. Create one object with employee details and print employee name.
+let employee = {
+    name: "Abi",
+    age: 30,
+    position: "Software Engineer"
+};
+console.log(employee.name);
+
+// 36. Create variable with string "100" and print typeof.
+let str= "100";
+console.log(typeof str);
+
+// 37. Create variable with number 100 and print typeof.
+let num5 = 100;
+console.log(typeof num5);
+
+// 38. Create one null variable and print it.
+let value = null;
+console.log(value);
+
+// 39. Create one array with mixed datatypes.
+let mixed = ["Hello", 42, true, null, undefined];
+
+// 40. Create object with student details:
+//     name, mark, passed(boolean)
+let student={
+    name:"raj",
+    mark:60,
+    passed: true
+}
+console.log(student);
